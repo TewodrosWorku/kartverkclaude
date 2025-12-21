5a79e203096d77bd794c414d1a7f1c865e7b54d7
unknown <tewodros.sebsibe@gmail.com>
2025-12-21 11:32:52 +0100

    size issue fixed for trafikkskilt

--- js/sign-manager.js
+++ js/sign-manager.js
@@ -914,8 +914,8 @@ export function placeSign(signId, latlng, rotation = 0, vertices = null) {
         iconOptions.iconAnchor = [16, 16]; // Center anchor for 32x32 - prevents zoom drift
     } else {
         // Trafikkskilt - programmatic size control (too many to resize manually)
-        iconOptions.iconSize = [40, 40];
-        iconOptions.iconAnchor = [20, 20]; // Center anchor
+        iconOptions.iconSize = [30, 30];
+        iconOptions.iconAnchor = [15, 15]; // Center anchor
     }
 
     const icon = L.icon(iconOptions);
